f2330c3236a56e9fef12a7da5f07a1f5a74777e3
jjc4480 <51353768+jjc4480@users.noreply.github.com>
2020-04-26 16:22:14 +0900

    add_middleware

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 //index.js
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on : http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Home");
+const handleProfile = (req, res) => res.send("Profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: ture }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
